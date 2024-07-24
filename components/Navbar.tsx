3d8fcdcfa2d305e668a0cf9bd8be6db17fc256bf
Syed Umair <syedumair.symits@gmail.com>
2024-07-24 17:21:03 +0500

    navbar in progress

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='border flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+      <Link href={'/'} className='border flex items-center gap-1'>
+        <Image src="/icons/logo.svg" alt="yoom_logo" className='max-sm:size-10' width={32} height={32}/>
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+      </Link>
+    </nav>
   )
 }
 
